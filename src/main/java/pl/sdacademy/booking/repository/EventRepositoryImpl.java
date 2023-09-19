e7cb20e91d36072aa390476bb421efc5049bfcab
think-gold <118820315+think-gold@users.noreply.github.com>
2023-09-19 15:10:41 +0200

    method findAll() in EventRepositoryImpl added

--- src/main/java/pl/sdacademy/booking/repository/EventRepositoryImpl.java
+++ src/main/java/pl/sdacademy/booking/repository/EventRepositoryImpl.java
@@ -1,4 +1,24 @@
 package pl.sdacademy.booking.repository;
 
-public class EventRepositoryImpl {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import pl.sdacademy.booking.data.EventEntity;
+import pl.sdacademy.booking.util.DatabaseUtil;
+
+import java.util.List;
+
+public class EventRepositoryImpl implements EventRepository{
+    private EntityManager entityManager;
+
+    public EventRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<EventEntity> findAll() {
+        TypedQuery<EventEntity> events = entityManager
+                .createQuery("select event from EventEntity event "
+                        , EventEntity.class);
+        return events.getResultList();
+    }
 }
