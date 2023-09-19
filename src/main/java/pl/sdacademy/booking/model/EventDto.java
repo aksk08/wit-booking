e92aa4c7412b524911ddb3c1738420684ca7fa02
think-gold <118820315+think-gold@users.noreply.github.com>
2023-09-19 14:37:05 +0200

    EventDto created

--- src/main/java/pl/sdacademy/booking/model/EventDto.java
+++ src/main/java/pl/sdacademy/booking/model/EventDto.java
@@ -1,4 +1,18 @@
 package pl.sdacademy.booking.model;
 
+import jakarta.persistence.Column;
+import pl.sdacademy.booking.data.ItemEntity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class EventDto {
+    //Dane z ItemEntity:
+    private String name;
+    private BigDecimal price;
+
+    //dane z EventEntity:
+    private ItemEntity item;
+    private LocalDateTime fromTime;
+    private LocalDateTime toTime;
 }
