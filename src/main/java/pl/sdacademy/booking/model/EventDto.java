a3bed4aabac61267fabac43895645b9ab550ef92
think-gold <118820315+think-gold@users.noreply.github.com>
2023-09-19 15:21:38 +0200

    EventService + findAll() method created

--- src/main/java/pl/sdacademy/booking/model/EventDto.java
+++ src/main/java/pl/sdacademy/booking/model/EventDto.java
@@ -1,18 +1,25 @@
 package pl.sdacademy.booking.model;
 
 import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import pl.sdacademy.booking.data.ItemEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
+@Builder
+@Getter
+@ToString
+@EqualsAndHashCode
 public class EventDto {
     //Dane z ItemEntity:
     private String name;
     private BigDecimal price;
 
     //dane z EventEntity:
-    private ItemEntity item;
+    //private ItemEntity item;
     private LocalDateTime fromTime;
     private LocalDateTime toTime;
 }
