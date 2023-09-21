16cf5867bcd36bf21de79a094882e78638881085
JudytaBak <131193456+JudytaBak@users.noreply.github.com>
2023-09-21 12:07:40 +0200

    NewEventValidator and Tests

---
+++ src/main/java/pl/sdacademy/booking/model/NewEventDto.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.booking.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NewEventDto {
+    private String itemName;
+    private LocalDateTime fromTime;
+    private LocalDateTime toTime;
+}
